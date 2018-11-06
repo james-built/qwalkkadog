38d7b0e7ff6591be4e17d7f36f0c826df9b840c8
james-built <jgbuilt@gmail.com>
2018-11-06 18:34:52 +1300

    add babel config, working on webpack config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const webpack = require ('webpack')
+const HTMLWebpackPlugin = require ('html-webpack-plugin
+
+const port = process.env.PORT || 3000
+
+module.exports = {
+  mode: 'development',
+  
+}
