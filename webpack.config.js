9c24ec33cc6abc966f37f3ad2c1c30293043aec8
james-built <jgbuilt@gmail.com>
2018-11-06 23:49:45 +1300

    webpack config

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require ('webpack')
-const HTMLWebpackPlugin = require ('html-webpack-plugin
-
-const port = process.env.PORT || 3000
-
-module.exports = {
-  mode: 'development',
-  
-}
